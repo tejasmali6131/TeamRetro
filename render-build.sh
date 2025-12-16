ff96b1ba158c6c133b57a3e2864851165b743151
MALI TEJAS (EXT) <tejas.mali@kone.com>
2025-12-16 10:55:50 +0530

    Include devDependencies in Render build script

--- render-build.sh
+++ render-build.sh
@@ -1,20 +1,16 @@
-# Build commands for Render deployment
-# Build command: ./render-build.sh
-# Start command: npm run start
-
 #!/usr/bin/env bash
 set -e
 
 echo "Installing frontend dependencies..."
 cd frontend
-npm install
+npm install --include=dev
 
 echo "Building frontend..."
 npm run build
 
 echo "Installing backend dependencies..."
 cd ../backend
-npm install
+npm install --include=dev
 
 echo "Building backend..."
 npm run build
